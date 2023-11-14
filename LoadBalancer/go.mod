f0086ba9771d95d31e9614bd893d3dab25bca3eb
tsbrandon1010 <tsbrandon1010@gmail.com>
2023-11-14 12:04:19 -0500

    New load balancer architecture

--- LoadBalancer/go.mod
+++ LoadBalancer/go.mod
@@ -1,3 +1,12 @@
 module main
 
 go 1.21.2
+
+require (
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
+	github.com/gomodule/redigo v1.8.4 // indirect
+	github.com/googollee/go-socket.io v1.7.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/zhouhui8915/engine.io-go v0.0.0-20150910083302-02ea08f0971f // indirect
+	github.com/zhouhui8915/go-socket.io-client v0.0.0-20200925034401-83ee73793ba4 // indirect
+)
--- LoadBalancer/main.go
+++ LoadBalancer/main.go
@@ -1,110 +1,51 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"sync"
-	"time"
+	"strconv"
+
+	socketio "github.com/zhouhui8915/go-socket.io-client"
 )
 
 type Server struct {
-	URL               *url.URL
-	Alive             bool
-	Mux               sync.RWMutex
-	ActiveConnections int
+	URL string
+	Connections int
 }
 
-type Config struct {
-	HealthCheckInterval string   `json:"healthCheckInterval"`
-	Servers             []string `json:"servers"`
-	Port                string   `json:"port"`
-}
-
-func loadConfig(file string) (Config, error) {
-	var config Config
-
-	bytes, err := ioutil.ReadFile(file)
+func createClient(server *Server) {	
+	client, err := socketio.NewClient(server.URL, &socketio.Options{})
 	if err != nil {
-		return config, err
+		log.Printf("Error while creating a client: %v", err)
+		return
 	}
 
-	err = json.Unmarshal(bytes, &config)
-	if err != nil {
-		return config, err
-	}
+	client.On("connection", func()  {
+		client.Emit("client-count-request")
+	})
 
-	return config, nil
-}
-
-func nextServer(servers []*Server) *Server {
-	leastActiveConnections := -1
-	leastActiveServer := servers[0]
-
-	for _, server := range servers {
-		server.Mux.Lock()
-		if (server.ActiveConnections < leastActiveConnections || leastActiveConnections == -1) && server.Alive {
-			leastActiveServer = server
+	client.On("client-count", func(msg string) {
+		clientCount, err := strconv.Atoi(msg)
+		if err != nil {
+			clientCount = server.Connections
 		}
-		server.Mux.Unlock()
-	}
-	return leastActiveServer
-}
+		server.Connections = clientCount
+	})
 
-func (s *Server) Proxy() *httputil.ReverseProxy {
-	return httputil.NewSingleHostReverseProxy(s.URL)
 }
 
 func main() {
 
-	config, err := loadConfig("config.json")
-	if err != nil {
-		log.Fatalf("Error loading the config file: %s", err.Error())
+	mainServer := &Server{
+		URL: "http://localhost:3000",
+		Connections: 0,
 	}
 
-	healthCheckInterval, err := time.ParseDuration(config.HealthCheckInterval)
-	if err != nil {
-		log.Fatalf("Invalid health check interval: %s", err.Error())
-	}
+	servers := [] *Server{mainServer}
 
-	var servers []*Server
-	for _, serverUrl := range config.Servers {
-		u, _ := url.Parse(serverUrl)
-		servers = append(servers, &Server{URL: u})
-	}
+	// check every server's client count based on a set period
+	// - at a given time interval
+	// first, we update the client count
+	// next, if the client count reaches a certain number, we "spin" up an additional server
 
-	for _, server := range servers {
-		go func(s *Server) {
-			for range time.Tick(healthCheckInterval) {
-				res, err := http.Get(s.URL.String())
-				if err != nil || res.StatusCode >= 500 {
-					s.Alive = false
-				} else {
-					s.Alive = true
-				}
-			}
-		}(server)
-	}
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		server := nextServer(servers)
-		server.Mux.Lock()
-		server.ActiveConnections++
-		server.Mux.Unlock()
-		server.Proxy().ServeHTTP(w, r)
-		log.Println("Sending User to: ", server.URL)
-		server.Mux.Lock()
-		server.ActiveConnections--
-		server.Mux.Unlock()
-	})
-
-	log.Println("Starting the load balancer on port ", config.Port)
-	err = http.ListenAndServe(config.Port, nil)
-	if err != nil {
-		log.Fatalf("Error while starting the load balancer: %s\n", err)
-	}
-
+	
 }
